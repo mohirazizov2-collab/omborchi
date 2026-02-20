dfbab63ccc1e2a48e4f9abb64879fe5ed173a417
XEM <f2472839@gmail.com>
2026-02-20 21:11:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4209846898-d5885",
+  "appId": "1:619088904904:web:aa308ecfe1fbbfcb551bfd",
+  "apiKey": "AIzaSyBU9fDCniS_vKWht2yg2wZ5-cfJg1-lPrY",
+  "authDomain": "studio-4209846898-d5885.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "619088904904"
+};
